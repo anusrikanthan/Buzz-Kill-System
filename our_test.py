07d47532b0d919bda8a02efba2e4a72a3b7da415
Danavishnu <41829973+Danavishnu@users.noreply.github.com>
2019-06-24 20:38:08 +0530

    Update our_test.py

--- our_test.py
+++ our_test.py
@@ -1,9 +1,10 @@
+// take attention values into consideration only if its above 0
 from NeuroPy import NeuroPy
 import time 
 import win32gui
 from twilio.rest import Client
 
-account_sid = 'AC0d982082455eff6334adaba25a77d04f'
+account_sid = 'AC0d982082455eff6334adaba25a77d04f'          // Change twilio account details to your own 
 auth_token = '3b3e2361f7fdb0ac94e4ba9d9fe30d56'
 client = Client(account_sid, auth_token)
 
@@ -20,7 +21,7 @@ print("3")
 # flag=1
 # lastAttention = 0
 while(True):
-    if(ob.attention > 0)and(ob.attention < 50):
+    if(ob.attention > 0)and(ob.attention < 50):                     // Sending alert if attention levels go below a certain level.
         message = client.messages.create(
             body='Attention levels loww !'+str(ob.attention),
             from_='+17738773065',
